58ae8143bb8921dab9b4fc150b51a1e64736a171
amyusif <am.yusif530@gmail.com>
2023-07-09 09:07:23 +0000

    implemented the line graph for 3 hour forcast

---
+++ src/components/HourForcast/HourForcast.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hour.css";
+import LineForcast from "../Line/Line";
+
+const HourForcast = ({ data }) => {
+  console.log(data);
+
+  return (
+    <div className="hour">
+    <h5>Hourly Forcast of the day</h5>
+     {data && <LineForcast dataFetch={data} />} 
+    </div>
+  );
+};
+
+export default HourForcast;
